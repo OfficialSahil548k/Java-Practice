41f00fad85303f143bfd226a7232dc88e320249e
Sahil Sharma <officialsahil548k@gmail.com>
2025-10-25 23:55:26 +0530

    solved trapped water problem

--- Arrays_2/_2.java
+++ Arrays_2/_2.java
@@ -2,6 +2,28 @@ package Arrays_2;
 
 public class _2 {
     public static void main(String[] args) {
-        
+        int heights[] = {4,2,0,6,3,2,5};
+        int maxWater = 0;
+        int left = 0, right = heights.length - 1;
+        int leftMax = 0, rightMax = 0;
+
+        while (left <= right) {
+            if (heights[left] <= heights[right]) {
+            if (heights[left] >= leftMax) {
+                leftMax = heights[left];
+            } else {
+                maxWater += leftMax - heights[left];
+            }
+            left++;
+            } else {
+            if (heights[right] >= rightMax) {
+                rightMax = heights[right];
+            } else {
+                maxWater += rightMax - heights[right];
+            }
+            right--;
+            }
+        }
+        System.out.println("Maximum water trapped: " + maxWater);
     }
 }
