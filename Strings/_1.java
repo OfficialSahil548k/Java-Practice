25d3b1f9dd6bb2cc09d3d70f03acfc2ec4274358
Sahil Sharma <officialsahil548k@gmail.com>
2025-12-17 19:06:51 +0530

    starting with strings

---
+++ Strings/_1.java
@@ -0,0 +1,10 @@
+package Strings;
+import java.util.*;
+public class _1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
